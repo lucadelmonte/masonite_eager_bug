cbee7e4081c21c3b2a7d41f92a84a5687f10e0d6
Luca Del Monte <luca.delmonte5@gmail.com>
2022-07-28 12:39:12 +0200

    impreved run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+echo "(• _ •)Executing migrations...(• _ •)"
 masonite-orm migrate --no-interaction --directory migrations --force
 
+echo  "(• _ •)Executing query with eager loading...(• _ •)"
 python3.10 main.py eager > /tmp/queries_eager.log
+echo  "(• _ •)Executing query without eager loading... (• _ •)"
 python3.10 main.py > /tmp/queries.log
 
-wc -l /tmp/queries_eager.log #12
-wc -l /tmp/queries.log #11
+echo "(• _ •)Comparing queries...(• _ •)"
+echo "Number of SELECT with eager loading: $(wc -l < /tmp/queries_eager.log)"
+echo "Number of SELECT without eager loading: $(wc -l < /tmp/queries.log)"
 
+echo "(• _ •)diff between queries:"
 diff /tmp/queries_eager.log /tmp/queries.log
 
+echo "(• _ •)Queries executed with eager loading:"
 cat /tmp/queries_eager.log
